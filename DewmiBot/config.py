461f9d784a465dfd8c22046266f84509af873a5f
Amrit Raj <87439773+amritrajj@users.noreply.github.com>
2022-06-16 17:32:50 +0530

    Update and rename sample_config.py to config.py

--- DewmiBot/config.py
+++ DewmiBot/config.py
@@ -18,13 +18,13 @@ class Config(object):
     API_HASH = "API_HASH"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 123456  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "hirunaoficial"
-    SUPPORT_CHAT = "sltechzoneofficial"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "hil"
+    SUPPORT_CHAT = "sltal"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
